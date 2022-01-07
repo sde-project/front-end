c8afd49dc1ca830bacd9bb8a29583721cf10c18a
MatteoZanotto <matteozanotto286@gmail.com>
2022-01-07 15:24:31 +0100

    badge on best exchanges

--- src/js/exchanges.js
+++ src/js/exchanges.js
@@ -1,1 +1,18 @@
 import { getAllBestExchanges } from "../modules/exchanges.js";
+
+const CRYPTOS = ['BTC', 'ETH', 'LTC', 'DOGE', 'DOT', 'SOL', 'XRP', 'LUNA', 'AVAX', 'BNB'];
+
+$(document).ready(() => {
+    getAllBestExchanges()
+    .then(resp => {
+        CRYPTOS.forEach(crypto => {
+            var bestBuy = resp[crypto]['buy'].exchange;
+            var bestBuyId = '#'+crypto.toLowerCase()+'-'+bestBuy.toLowerCase().replace('.', '');
+            $(bestBuyId).append('<span class="badge bg-danger rounded-pill">best buy!</span>');
+            var bestSell = resp[crypto]['sell'].exchange;
+            var bestSellId = '#'+crypto.toLowerCase()+'-'+bestSell.toLowerCase().replace('.', '');
+            $(bestSellId).append('<span class="badge bg-success rounded-pill">best sell!</span>');
+        })
+    })
+    .catch(err => console.log(err));
+});
--- src/modules/exchanges.js
+++ src/modules/exchanges.js
@@ -1,5 +1,7 @@
 import http from './http.js';
 
 export function getAllBestExchanges () {
-    
+    return http.get('https://cryptodashboard.it/exchanges/best/all')
+    .then(response => response.json())
+    .catch(err => console.log(err));
 }
